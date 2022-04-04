950565d473e79b42afbf886187bd2ec9536b997d
Aldhafiri70 <thelordshoes@Gmail.com>
2022-04-04 22:06:01 +0300

    done

--- All in one/All in one/Views/brandview.swift
+++ All in one/All in one/Views/brandview.swift
@@ -26,66 +26,78 @@ brands(brandimagename: "Mercedes"),]
 //            }.listStyle(.plain)
                 ScrollView(){
                     VStack(alignment: .leading){
-                    HStack{
+            
                     NavigationLink(destination:Nike()){
+                        HStack{
             Image("\(brandsArray[0].brandimagename)").resizable().scaledToFit().frame(width:150, height: 150)
-                    }
+                    
                         Text("NIKE")
                             .font(.title)
                             .foregroundColor(Color.orange)
                         Spacer()
                 Image(systemName:"chevron.right.circle").foregroundColor(Color.white)
+                        }
                     }
-                    HStack{
+                   
                     NavigationLink(destination:adidas()){
+                        HStack{
             Image("\(brandsArray[1].brandimagename)").resizable().scaledToFit().frame(width:150, height: 150)
-                }
+                
                         Text("Adidas")
                             .font(.title)
                             .foregroundColor(Color.white)
                         Spacer()
                 Image(systemName:"chevron.right.circle").foregroundColor(Color.white)
+                        }
                     }
-                    HStack{
+                    
                     NavigationLink(destination:Rolex()){
+                        HStack{
             Image("\(brandsArray[2].brandimagename)").resizable().scaledToFit().frame(width:150, height: 150)
-                }
+                
                         Text("Rolex")
                             .font(.title)
                             .foregroundColor(Color.green)
                         Spacer()
                 Image(systemName:"chevron.right.circle").foregroundColor(Color.white)
                     }
-                    HStack{
+                    }
+                   
                     NavigationLink(destination:RhicardMille()){
+                        HStack{
             Image("\(brandsArray[3].brandimagename)").resizable().scaledToFit().frame(width:150, height: 150)
-                }
+                
                         Text("RM")
                             .font(.title)
                             .foregroundColor(Color.gray)
                         Spacer()
                 Image(systemName:"chevron.right.circle").foregroundColor(Color.white)
+                        }
                     }
-                    HStack{
+                    
                     NavigationLink(destination:BMW()){
+                        HStack{
             Image("\(brandsArray[4].brandimagename)").resizable().scaledToFit().frame(width:150, height: 150)
-                }
+                
+                    
                         Text("BMW")
                             .font(.title)
                             .foregroundColor(Color.blue)
                         Spacer()
                 Image(systemName:"chevron.right.circle").foregroundColor(Color.white)
-                        
+                        }
                     }
-                        HStack{
+                       
                     NavigationLink(destination:Mercedes()){
+                        HStack{
             Image("\(brandsArray[5].brandimagename)").resizable().scaledToFit().frame(width:150, height: 150)
-                }
+                
                             Text("Mercedes")
                                 .font(.title)
                                 .foregroundColor(Color("prim6"))
                             Spacer()
                     Image(systemName:"chevron.right.circle").foregroundColor(Color.white)
+                        }
                         }
                 }
             }.navigationBarHidden(true)
